8fa5373cdb3753fd7da3b9b0c7d96770ac7d7c1b
pszczepanski <pszczepanski@webtechnika.pl>
2020-08-11 12:57:49 +0200

    Finally working integration of VPN with pypi

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
 rm /etc/network/if-up.d/debug-up
 rm /etc/network/if-post-down.d/debug-down
-rm /etc/network/if-up.d/pypi-down
-rm /etc/network/if-post-down.d/pypi-up
+rm /etc/network/if-up.d/pypi-up
+rm /etc/network/if-post-down.d/pypi-down
--- install.py
+++ install.py
@@ -1,4 +1,6 @@
 import shutil
+import os
+import stat
 
 
 def string_replacer(filename, string, to):
@@ -10,12 +12,26 @@ def string_replacer(filename, string, to):
         file.write(filedata)
 
 
+def chmod_x(file):
+    """Give +x permission to file."""
+    st = os.stat(file)
+    os.chmod(file, st.st_mode | stat.S_IEXEC)
+
+
 with open('vpnid.txt') as f:
-    connection_id = f.readlines()[0]
+    content = f.readlines()
+    user = content[0].rstrip()
+    connection_id = content[1].rstrip()
+string_replacer('pypi-down', '___USER___', user)
 string_replacer('pypi-down', '___VPN___ID___', connection_id)
-shutil.copyfile('./debug-up', '/etc/network/if-up.d/debug-up')
+shutil.copyfile('./pypi-down', '/etc/network/if-post-down.d/pypi-down')
+chmod_x('/etc/network/if-post-down.d/pypi-down')
+string_replacer('pypi-up', '___USER___', user)
 string_replacer('pypi-up', '___VPN___ID___', connection_id)
-shutil.copyfile('./debug-up', '/etc/network/if-up.d/debug-up')
+shutil.copyfile('./pypi-up', '/etc/network/if-up.d/pypi-up')
+chmod_x('/etc/network/if-up.d/pypi-up')
 string_replacer('pypi-down', connection_id, '___VPN___ID___')
 string_replacer('pypi-up', connection_id, '___VPN___ID___')
+string_replacer('pypi-down', user, '___USER___')
+string_replacer('pypi-up', user, '___USER___')
 print('Moved files successfully.')
